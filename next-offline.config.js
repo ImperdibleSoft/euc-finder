d69c3db95cccdf31c2279724f0b4f30de05f9b05
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2021-10-14 12:18:31 +0200

    Improve service-worker caching definitions

--- next-offline.config.js
+++ next-offline.config.js
@@ -1,25 +1,52 @@
+const assetsPattern = /.*\.(png|PNG|jpeg|JPEG|jpg|JPG|gif|GIF)$/gm;
+const bundlesPattern = /.*\.(html|css|js|woff2)$/gm;
+const documentsPattern = /https:\/\/www.eucfinder.com\/((en|es)(\/)?)?(wheels\/.*)?$/gm;
+
+const CacheFirst = 'CacheFirst';
+const NetworkFirst = 'NetworkFirst';
+
+const commonOptions = {
+  cacheableResponse: { statuses: [0, 200] },
+  expiration: { maxAgeSeconds: 365 * 24 * 60 * 60 }
+};
+
+const assets = {
+  urlPattern: assetsPattern,
+  handler: CacheFirst,
+  options: {
+    cacheName: 'assets',
+    cacheableResponse: commonOptions.cacheableResponse,
+    expiration: commonOptions.expiration
+  }
+};
+
+const bundles = {
+  urlPattern: bundlesPattern,
+  handler: NetworkFirst,
+  options: {
+    cacheName: 'bundles',
+    cacheableResponse: commonOptions.cacheableResponse,
+    expiration: commonOptions.expiration
+  }
+};
+
+const documents = {
+  urlPattern: documentsPattern,
+  handler: NetworkFirst,
+  options: {
+    cacheName: 'bundles',
+    cacheableResponse: commonOptions.cacheableResponse,
+    expiration: commonOptions.expiration
+  }
+};
+
 module.exports = {
   workboxOpts: {
     swDest: '../public/service-worker.js',
     runtimeCaching: [
-      {
-        urlPattern: /.(png|PNG|jpeg|JPEG|jpg|JPG|gif|GIF)$/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'assets',
-          cacheableResponse: { statuses: [0, 200] },
-          expiration: { maxAgeSeconds: 365 * 24 * 60 * 60 }
-        }
-      },
-      {
-        urlPattern: /.(html|css|js|woff2)$/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'bundles',
-          cacheableResponse: { statuses: [0, 200] },
-          expiration: { maxAgeSeconds: 365 * 24 * 60 * 60 }
-        }
-      }
+      assets,
+      bundles,
+      documents
     ]
   }
 };
