b08b2e2ab552286ccc656e0b817aae77259e93f7
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2021-11-07 16:11:49 +0100

    Add page scrappers

---
+++ utils/scrapper/index.ts
@@ -0,0 +1,4 @@
+export * from './ciclonic';
+export * from './inmotionFrance';
+export * from './myewheel';
+export * from './urban360';
---
+++ utils/scrapper/inmotionFrance.ts
@@ -0,0 +1,33 @@
+import { JSDOM } from 'jsdom';
+
+const priceRegExp = /([0-9]{1,},[0-9]{2,2})/;
+
+export const parseInmotionFrancePrice = (html: string): number | undefined => {
+  try {
+    const dom = new JSDOM(html);
+    const { document } = dom.window;
+
+    /** Sold out. Out of stock */
+
+    /** Final price when released */
+
+    /** Sale price */
+  
+    /** Regular price */
+    const [
+      cheapPriceElement
+    ] = Array.from(document.querySelectorAll('.product-price .current-price')) ?? [];
+    if (cheapPriceElement) {
+      const cheapString = cheapPriceElement?.innerHTML?.replace('.', '');
+      const [, rawCheap] = cheapString?.match(priceRegExp) ?? [];
+      const cheap = rawCheap?.replace(',', '.');
+      if (cheap) {
+        return Number(cheap);
+      }
+    }
+
+    return undefined;
+  } catch (error) {
+    return undefined;
+  }
+};
