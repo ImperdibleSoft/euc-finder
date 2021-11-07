facd17e7156e08e0aa3a2a26d91bf1e537123319
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2021-11-07 16:19:30 +0100

    Implement wheelPrices hooks

--- components/PurchaseLink/index.tsx
+++ components/PurchaseLink/index.tsx
@@ -1,20 +1,40 @@
 import { Box, Button, Card, CardMedia, Icon, Typography } from '@mui/material';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useConfirmationModal } from '../../hooks';
+import { useConfirmationModal, useWheelPrice, useWheelPriceStyles } from '../../hooks';
 import { Store } from '../../types';
-import { isDarkTheme } from '../../utils';
+import { currency, isDarkTheme } from '../../utils';
 
 interface Props {
   discount?: number;
+  expensive: boolean;
   large?: boolean;
   url: string;
   store: Store;
 }
 
-const PurchaseLink: React.FC<Props> = ({ discount, large = false, url, store }) => {
+// eslint-disable-next-line max-lines-per-function
+const PurchaseLink: React.FC<Props> = ({ discount, expensive, large = false, url, store }) => {
   const { t } = useTranslation();
   const dark = isDarkTheme();
+  const { loadingState, price: rawPrice } = useWheelPrice(store.id, url, expensive);
+
+  const [price, discountedPrice] = useMemo(() => {
+    if (!rawPrice || rawPrice === '-') {
+      return [];
+    }
+
+    const p = currency(rawPrice);
+    if (!discount) {
+      return [p];
+    }
+
+    const dp = rawPrice - (rawPrice * discount / 100);
+    return [p, currency(dp)];
+  }, [discount, rawPrice]);
+
+  const mainStyles = useWheelPriceStyles(true, large);
+  const secondaryStyles = useWheelPriceStyles(false, large);
   
   const navigate = () => {
     window?.open(url);
@@ -69,6 +89,64 @@ const PurchaseLink: React.FC<Props> = ({ discount, large = false, url, store }) 
           <Typography variant="body1" component="span" sx={ { alignItems: 'center', display: 'flex', mb: 2 } }>
             <Icon sx={ { mr: 1 } } color="secondary">local_offer</Icon> { t('discount', { discount }) }
           </Typography>
+        ) }
+
+        { loadingState === 'loading' && (
+          <Typography variant="body1" component="span" sx={ { mb: 2 } }>
+            { t('loadingPrice-msg') }
+          </Typography>
+        ) }
+
+        { loadingState === 'success' && rawPrice === '-' && (
+          <Typography
+            variant={ (discountedPrice ? secondaryStyles : mainStyles).variant }
+            component="span"
+            sx={ {
+              color: (theme) => theme.palette.grey[500],
+              fontWeight: mainStyles.fontWeight,
+              mb: 2,
+              textDecoration: mainStyles.textDecoration
+            } }
+          >
+            { t('soldOut-msg') }
+          </Typography>
+        ) }
+
+        { loadingState !== 'loading' && !!rawPrice && rawPrice !== '-' && (
+          <Box
+            sx={ {
+              alignItems:  'center',
+              display: 'flex',
+              flexDirection: 'column',
+              mb: 2
+            } }
+          >
+            <Typography
+              variant={ (discountedPrice ? secondaryStyles : mainStyles).variant }
+              component="span"
+              sx={ {
+                color: (theme) => theme.palette.grey[500],
+                fontWeight: (discountedPrice ? secondaryStyles : mainStyles).fontWeight,
+                textDecoration: (discountedPrice ? secondaryStyles : mainStyles).textDecoration
+              } }
+            >
+              { price }
+            </Typography>
+
+            { !!discountedPrice && (
+              <Typography
+                variant={ mainStyles.variant }
+                component="span"
+                sx={ {
+                  color: (theme) => theme.palette.secondary.main,
+                  fontWeight: mainStyles.fontWeight,
+                  textDecoration: mainStyles.textDecoration
+                } }
+              >
+                { discountedPrice }
+              </Typography>
+            ) }
+          </Box>
         ) }
 
         <Button
