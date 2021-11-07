1b2ec145bad5ab8563a12ec03b867b63e522ed7e
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2021-11-07 16:15:06 +0100

    Add eucfinder API config

---
+++ apis/eucfinder/index.ts
@@ -0,0 +1,48 @@
+import { http } from '../../utils';
+
+const getCiclonicPrice = async (url: string, expensive: boolean): Promise<number | undefined> => {
+  try {
+    const expensiveParam = expensive ? '?expensive=true' : '';
+    const response = await http.get(`/api/ciclonic/${ encodeURIComponent(url) }${ expensiveParam }`);
+    return response.data as (number | undefined);
+  } catch {
+    return undefined;
+  }
+};
+
+const getInmotionFrancePrice = async (url: string): Promise<number | undefined> => {
+  try {
+    const response = await http.get(`/api/inmotionFrance/${ encodeURIComponent(url) }`);
+    return response.data as (number | undefined);
+  } catch {
+    return undefined;
+  }
+};
+
+const getMyEWheelPrice = async (url: string): Promise<number | '-' | undefined> => {
+  try {
+    const response = await http.get(`/api/myewheel/${ encodeURIComponent(url) }`);
+    return response.data as number | '-' | undefined;
+  } catch {
+    return undefined;
+  }
+};
+
+const getUrban360Price = async (url: string, expensive: boolean): Promise<number | undefined> => {
+  try {
+    const expensiveParam = expensive ? '?expensive=true' : '';
+    const response = await http.get(`/api/urban360/${ encodeURIComponent(url) }${ expensiveParam }`);
+    return response.data as (number | undefined);
+  } catch {
+    return undefined;
+  }
+};
+
+const eucFinderApi = {
+  getCiclonicPrice,
+  getInmotionFrancePrice,
+  getMyEWheelPrice,
+  getUrban360Price
+};
+
+export default eucFinderApi;
