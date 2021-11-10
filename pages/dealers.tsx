3016ce2769c51bbca3ef72564e69c5d2fc0cfb9d
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2021-11-10 09:56:21 +0100

    Improvement: In dealers page, only display dealers for selected region

--- pages/dealers.tsx
+++ pages/dealers.tsx
@@ -5,16 +5,37 @@ import { useTranslation } from 'react-i18next';
 import DealerCard from '../components/DealerCard';
 import SimpleLayout from '../components/Layouts/SimpleLayout';
 import { APP_NAME } from '../constants';
+import { useArenaContext } from '../context';
 // @ts-ignore
 import dealersMarkdown from '../docs/dealers.md';
+import { Region } from '../types';
 import { parseMarkdown } from '../utils/dealers';
 import { getStaticProps } from '../utils/serverTranslatedResources';
 
+const getRegionId = (regionCode: Region) => {
+  switch (regionCode) {
+    case 'eu':
+      return 'europe';
+
+    case 'us':
+      return 'america';
+
+    case 'asia':
+    default:
+      return regionCode;
+  }
+};
+
 const Dealers: React.FC = () => {
   const { t } = useTranslation();
-  const dealers = parseMarkdown(dealersMarkdown);
+  const { region: regionCode } = useArenaContext();
+  const regionId = getRegionId(regionCode);
+
+  const regions = parseMarkdown(dealersMarkdown);
+  const region = regions.find(r => r.name === regionId);
 
   const pageTitle = t('dealers-title');
+  const regionName = t(`${ regionId }-label`);
 
   return (
     <>
@@ -28,7 +49,7 @@ const Dealers: React.FC = () => {
         </Typography>
 
         <Typography variant="body1" component="p" sx={ { mb: 3 } }>
-          { t('dealers1-msg', { appName: APP_NAME }) }
+          { t('dealers1-msg', { appName: APP_NAME, region: regionName }) }
         </Typography>
 
         <Typography variant="body1" component="p" sx={ { mb: 3 } }>
@@ -47,23 +68,27 @@ const Dealers: React.FC = () => {
           { t('discounts2-msg') }
         </Typography>
 
-        { dealers.map(region => (
-          <Box key={ region.name } sx={ { mb: 5 } }>
-            <Typography variant="h5" component="h4" sx={ { mb: 2 } }>
-              { t(`${ region.name }-label`) }
-            </Typography>
+        <Box>
+          <Typography variant="h5" component="h4" sx={ { mb: 2 } }>
+            { regionName }
+          </Typography>
 
-            <Grid container spacing={ 2 }>
-              { region.dealers.map(({ dealerName, ...props }) => (
-                <DealerCard
-                  key={ dealerName }
-                  storeName={ dealerName }
-                  { ...props }
-                />
-              )) }
-            </Grid>
-          </Box>
-        )) }
+          <Grid container spacing={ 2 }>
+            { !region?.dealers.length && (
+              <Grid item xs={ 12 } sm={ 12 } md={ 12 } lg={ 12 } xl={ 12 }>
+                { t('noDealers-msg', { region: regionName }) }
+              </Grid>
+            ) }
+
+            { region?.dealers?.map(({ dealerName, ...props }) => (
+              <DealerCard
+                key={ dealerName }
+                storeName={ dealerName }
+                { ...props }
+              />
+            )) }
+          </Grid>
+        </Box>
       </SimpleLayout>
     </>
   );
