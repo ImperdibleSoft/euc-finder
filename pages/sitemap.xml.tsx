214d02519ccf2291d09eca2269e3af243cf2e29a
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2021-11-12 10:50:27 +0100

    Try to fix sitemap.xml

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -42,17 +42,15 @@ export const getServerSideProps = ({ res }: Props) => {
   const staticPages = [...mainPages, ...wheelsPages];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${ staticPages.map((url) => `
-        <url>
-          <loc>${ url }</loc>
-          <lastmod>${ new Date().toISOString() }</lastmod>
-          <changefreq>monthly</changefreq>
-          <priority>1.0</priority>
-        </url>
-      `).join('') }
-    </urlset>
-  `;
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${ staticPages.map((url) => `<url>
+      <loc>${ url }</loc>
+      <lastmod>${ new Date().toISOString() }</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>1.0</priority>
+    </url>
+    `).join('') }
+  </urlset>`;
 
   res.setHeader('Content-Type', 'text/xml');
   res.write(sitemap);
