b48fd2fb354b968d6e6497134e60f04a748b24b0
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2021-11-26 12:45:49 +0100

    Add some sample data

--- context/data/index.ts
+++ context/data/index.ts
@@ -1,5 +1,7 @@
 export * from './brands';
+export * from './influencers';
 export * from './stores';
+export * from './videos';
 export * from './wheelPictures';
 export * from './wheelPurchaseLinks';
 export * from './wheels';
