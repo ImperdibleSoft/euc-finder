b4a54ced0974fe3e6ce59910b3ac0a7532b84e0a
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2021-12-06 22:46:26 +0100

    Improve how VideoCard is rendered

--- components/VideoCard/index.tsx
+++ components/VideoCard/index.tsx
@@ -1,13 +1,15 @@
-import { Card, CardContent, Chip, List, ListItem } from '@mui/material';
+import { Avatar, Card, CardContent, Chip, List, ListItem } from '@mui/material';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useVideoInfo } from '../../hooks';
 import { Video } from '../../types';
 import YoutubePlayer from '../YoutubePlayer';
 
+const cardWidth = 314;
+
 const commonProps = {
   size: 'medium',
-  sx: { mr: 1 },
+  sx: { mb: 1, mr: 1 },
   variant: 'outlined'
 };
 
@@ -18,12 +20,12 @@ interface Props {
 const VideoCard: React.FC<Props> = ({ video }) => {
   const { t } = useTranslation();
   const { categories, influencers, url, wheels } = useVideoInfo(video);
-
-
+  
   return (
     <div
       style={ {
         display: 'inline-block',
+        maxWidth: cardWidth,
         paddingLeft: 16,
         paddingTop: 16,
         position: 'relative',
@@ -34,21 +36,23 @@ const VideoCard: React.FC<Props> = ({ video }) => {
         <YoutubePlayer 
           controls
           url={ url }
-          width={ 314 }
+          width={ cardWidth }
         />
 
         <CardContent
           sx={ {
+            maxWidth: '100%',
             p: 0,
             '&:last-child': { pb: 0 }
           } }
         >
           <List dense>
-            <ListItem>
+            <ListItem sx={ { display: 'block' } }>
               { influencers.map(influencer => (
                 // @ts-ignore
                 <Chip
                   key={ influencer.id }
+                  avatar={ <Avatar alt={ influencer.name } src={ influencer.avatar } /> }
                   color="secondary"
                   label={ influencer.name }
                   { ...commonProps }
