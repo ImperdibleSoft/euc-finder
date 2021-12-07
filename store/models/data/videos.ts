8ef9c9581028493d15bc0e0afd4ceaaa1003d74b
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2021-12-07 13:33:02 +0100

    Add more videos

--- store/models/data/videos.ts
+++ store/models/data/videos.ts
@@ -1,16 +1,25 @@
 import { VideoCategory, InfluencerId, Video, WheelId } from '../../../types';
+import eeveesVideos from './videos/eevees';
+import electricDreamsVideos from './videos/electricDreams';
+import eucVibesVideos from './videos/eucVibes';
 
 export const videos: Video[] = [
   {
     url: 'https://www.youtube.com/watch?v=84czA_ugr28',
-    tags: [InfluencerId.wrongWay, VideoCategory.review, VideoCategory.offroad, WheelId.exnHT]
+    tags: [InfluencerId.wrongWay, VideoCategory.review, VideoCategory.offroad, WheelId.exnHT],
+    date: new Date('01/Nov/2021')
   },
   {
     url: 'https://www.youtube.com/watch?v=MUtHWox6fys',
-    tags: [InfluencerId.ox, VideoCategory.review, VideoCategory.commuting, WheelId.v12]
+    tags: [InfluencerId.ox, VideoCategory.review, VideoCategory.commuting, WheelId.v12],
+    date: new Date('20/May/2021')
   },
   {
     url: 'https://www.youtube.com/watch?v=yB325Y0Unbg',
-    tags: [InfluencerId.kuji, VideoCategory.review, VideoCategory.commuting, WheelId.v11]
-  }
+    tags: [InfluencerId.kuji, VideoCategory.review, VideoCategory.commuting, WheelId.v11],
+    date: new Date('29/May/2020')
+  },
+  ...eeveesVideos,
+  ...electricDreamsVideos,
+  ...eucVibesVideos
 ];
---
+++ store/models/data/videos/electricDreams.ts
@@ -0,0 +1,178 @@
+import { InfluencerId, Video, VideoCategory, WheelId } from '../../../../types';
+
+const electricDreamsVideos: Video[] = [
+  {
+    url: 'https://www.youtube.com/watch?v=f6yNlqX9Nzs',
+    tags: [
+      InfluencerId.electricDreams,
+      VideoCategory.comparision,
+      WheelId.ks18l,
+      WheelId.msx
+    ],
+    date: new Date('28/Aug/2018')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=zik7TFIz4ng',
+    tags: [
+      InfluencerId.electricDreams,
+      VideoCategory.review,
+      VideoCategory.commuting,
+      WheelId.mten
+    ],
+    date: new Date('05/Sep/2019')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=P9xwUee8ULY',
+    tags: [
+      InfluencerId.electricDreams,
+      InfluencerId.evx,
+      VideoCategory.chatting,
+      WheelId.v11
+    ],
+    date: new Date('04/Apr/2020')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=rEIy8sMa4Hg',
+    tags: [
+      InfluencerId.electricDreams,
+      InfluencerId.evx,
+      InfluencerId.wrongWay,
+      VideoCategory.chatting,
+      VideoCategory.comparision,
+      WheelId.v11,
+      WheelId.ksS18
+    ],
+    date: new Date('15/Apr/2020')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=jdaR76AhLRc',
+    tags: [
+      InfluencerId.electricDreams,
+      VideoCategory.review,
+      VideoCategory.commuting,
+      WheelId.sherman
+    ],
+    date: new Date('21/Jun/2020')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=PDqZCfi3LMw',
+    tags: [
+      InfluencerId.electricDreams,
+      VideoCategory.review,
+      VideoCategory.commuting,
+      WheelId.sherman
+    ],
+    date: new Date('08/Jul/200')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=jNq2J9AdQlg',
+    tags: [
+      InfluencerId.electricDreams,
+      VideoCategory.review,
+      WheelId.ex,
+      WheelId.monsterPro,
+      WheelId.ksS18,
+      WheelId.v11
+    ],
+    date: new Date('19/Aug/2020')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=Hre7iRQyGlU',
+    tags: [
+      InfluencerId.electricDreams,
+      VideoCategory.comparision,
+      WheelId.sherman,
+      WheelId.exnHT,
+      WheelId.exnHS
+    ],
+    date: new Date('02/Jun/2021')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=lUzNWTBLYcw',
+    tags: [InfluencerId.electricDreams, VideoCategory.reveal, WheelId.v12],
+    date: new Date('05/Jun/2021')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=eBRG-9quvZE',
+    tags: [
+      InfluencerId.electricDreams,
+      VideoCategory.review,
+      VideoCategory.commuting,
+      WheelId.v12
+    ],
+    date: new Date('15/Jun/2021')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=wW9WClFfTvE',
+    tags: [
+      InfluencerId.electricDreams,
+      VideoCategory.review,
+      VideoCategory.commuting,
+      WheelId.v12
+    ],
+    date: new Date('16/Jun/2021')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=_q_pb-cTDtA',
+    tags: [
+      InfluencerId.electricDreams,
+      VideoCategory.review,
+      VideoCategory.commuting,
+      WheelId.v12
+    ],
+    date: new Date('18/Jun/2021')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=CdQY9p6dQV0',
+    tags: [
+      InfluencerId.electricDreams,
+      VideoCategory.comparision,
+      WheelId.v12,
+      WheelId.ks16x
+    ],
+    date: new Date('21/Jun/2021')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=dswS1CpXC4Q',
+    tags: [
+      InfluencerId.electricDreams,
+      VideoCategory.reveal,
+      WheelId.commanderHS,
+      WheelId.commanderHT
+    ],
+    date: new Date('21/Sep/2021')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=yOHpV6-nQDI',
+    tags: [InfluencerId.electricDreams, VideoCategory.reveal, WheelId.ksS20],
+    date: new Date('02/Oct/2021')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=FtDWsT3XncA',
+    tags: [
+      InfluencerId.electricDreams,
+      VideoCategory.review,
+      WheelId.commanderHS,
+      WheelId.commanderHT
+    ],
+    date: new Date('22/Oct/2021')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=ASjM8yjsviU',
+    tags: [InfluencerId.electricDreams, VideoCategory.commuting, WheelId.ksS20],
+    date: new Date('29/Oct/2021')
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=339nsbpbNxk',
+    tags: [
+      InfluencerId.electricDreams,
+      VideoCategory.review,
+      VideoCategory.commuting,
+      VideoCategory.offroad,
+      WheelId.hero
+    ],
+    date: new Date('25/Nov/2021')
+  }
+];
+
+export default electricDreamsVideos;
