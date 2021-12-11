1541fdb3ace7ae2b95448ff75ec5fd951a4cb28f
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2021-12-11 14:19:59 +0100

    Render a nice Influencers' page

--- pages/influencers/index.tsx
+++ pages/influencers/index.tsx
@@ -1,8 +1,9 @@
-import { Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { getStaticProps } from '..';
+import InfluencerCard from '../../components/Influencers/InfluencerCard';
 import SimpleLayout from '../../components/Layouts/SimpleLayout';
 import { getInfluencers } from '../../store/selectors';
 
@@ -12,15 +13,18 @@ const Influencers: React.FC = () => {
 
   return (
     <SimpleLayout>
-      <Typography variant="h4">
-        { t('influencers-label') }
+      <Typography variant="h4" sx={ { mb: 2 } }>
+        { t('influencers') }
       </Typography>
 
-      { influencers.map(influencer => (
-        <div key={ influencer.id }>
-          { influencer.name }
-        </div>
-      )) }
+      <Grid container spacing={ 2 }>
+        { influencers.map(influencer => (
+          <InfluencerCard
+            key={ influencer.id }
+            influencer={ influencer }
+          />
+        )) }
+      </Grid>
     </SimpleLayout>
   );
 };
