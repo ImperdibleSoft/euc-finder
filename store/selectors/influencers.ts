6e561968c88bde925c45b780b39b18f972fb8294
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-01-02 19:34:36 +0100

    Display sponsored influencers first

--- store/selectors/influencers.ts
+++ store/selectors/influencers.ts
@@ -2,7 +2,10 @@ import { InfluencerId } from '../../types';
 import { RootState } from '../types';
 
 export const getInfluencers = ({ influencers }: RootState) =>
-  influencers.collection;
+  [
+    ...influencers.collection.filter(influencer => influencer.sponsored),
+    ...influencers.collection.filter(influencer => !influencer.sponsored)
+  ];
 
 export const getInfluencerById = (id: InfluencerId) =>
   ({ influencers }: RootState) =>
