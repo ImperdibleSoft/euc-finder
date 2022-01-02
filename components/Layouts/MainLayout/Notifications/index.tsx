a74e50be07c778f7ac62f41be23cbd8293beba7c
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-01-02 20:05:40 +0100

    Improve floating icon

--- components/Layouts/MainLayout/Notifications/index.tsx
+++ components/Layouts/MainLayout/Notifications/index.tsx
@@ -1,4 +1,4 @@
-import { Icon, SpeedDial, SpeedDialAction } from '@mui/material';
+import { Fab, Icon } from '@mui/material';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { BOTTOM_NAVIGATION_HEIGHT } from '../../constants';
@@ -13,25 +13,24 @@ const Notifications: React.FC<Props> = ({ isTablet }) => {
   const navigateToGroup = () => {
     window.open('https://t.me/EUCFinder');
   };
-  
+
   return (
-    <SpeedDial
-      ariaLabel={ t('stayUpdated-msg') }
-      FabProps={ { sx: { backgroundColor: ({ palette }) => palette.secondary.main } } }
-      icon={ <Icon >notifications</Icon> }
+    <Fab
+      variant={ isTablet ? 'extended' : 'circular' }
+      size="large"
+      color="secondary"
+      aria-label="add"
       sx={ {
         position: 'fixed',
         bottom: 16 + (isTablet ? 0 : BOTTOM_NAVIGATION_HEIGHT),
         right: 16
       } }
+      onClick={ navigateToGroup }
     >
-      <SpeedDialAction
-        icon={ <Icon>telegram</Icon> }
-        onClick={ navigateToGroup }
-        tooltipOpen
-        tooltipTitle={ t('joinToTelegram-msg') }
-      />
-    </SpeedDial>
+      <Icon sx={ { mr: isTablet ? 1 : 0 } }>telegram</Icon>
+
+      { isTablet && t('joinToTelegram-msg') }
+    </Fab>
   );
 };
 
