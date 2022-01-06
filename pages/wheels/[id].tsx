6095dcd8ac5b1edcf64607d22f15928fe521599c
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-01-06 02:55:35 +0100

    Add FacebookComments to Wheel Detail page

--- pages/wheels/[id].tsx
+++ pages/wheels/[id].tsx
@@ -9,6 +9,7 @@ import EmptyCase from '../../components/EmptyCase';
 import SimpleLayout from '../../components/Layouts/SimpleLayout';
 import AdditionalPurchaseLinks from '../../components/WheelDetails/AdditionalPurchaseLinks';
 import AdditionalSpecs from '../../components/WheelDetails/AdditionalSpecs';
+import FacebookComments from '../../components/WheelDetails/Comments';
 import Header from '../../components/WheelDetails/Header';
 import HighlightedSpecs from '../../components/WheelDetails/HighlightedSpecs';
 import MainSpecs from '../../components/WheelDetails/MainSpecs';
@@ -119,7 +120,12 @@ const EucDetail: React.FC<Props> = ({ pictures }) => {
                 items={ regularLinks }
                 wheel={ id }
               />
+
+              <Grid item xs={ 12 }>
+                <FacebookComments />
+              </Grid>
             </Grid>
+
           </>
         ) }
       </SimpleLayout>
