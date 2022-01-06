93041578e544a66ff05743e812d5fc045035195e
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-01-06 02:48:01 +0100

    Add useResize hook

--- hooks/theme.ts
+++ hooks/theme.ts
@@ -1,4 +1,5 @@
 import { useMediaQuery, useTheme } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 
 export const useBreakpoints = () => {
   const theme = useTheme();
@@ -10,4 +11,70 @@ export const useBreakpoints = () => {
     sm,
     md
   };
+};
+
+interface Sizes {
+  screenWidth: number;
+  screenHeight: number;
+  ratiowh: number;
+  ratiohw: number;
+  rect?: {
+    width: number;
+    height: number;
+    left: number;
+    right: number;
+    top: number;
+    bottom: number;
+  };
+}
+
+export const useResize = (element?: React.RefObject<HTMLDivElement>) => {
+  const [sizes, setSizes] = useState<Sizes>({
+    screenWidth: 0,
+    screenHeight: 0,
+    ratiowh: 0,
+    ratiohw: 0,
+    rect: undefined
+  });
+
+  const onResize = () => {
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    const ratiowh = screenWidth / screenHeight;
+    const ratiohw = screenHeight / screenWidth;
+    let rect: Sizes['rect'];
+
+    if (element?.current) {
+      // DOM API does not allow for a shallow copy, so we have to manually set them
+      const clientRect = element.current.getBoundingClientRect();
+      rect = {
+        width: clientRect.width,
+        height: clientRect.height,
+        left: clientRect.left,
+        right: clientRect.right,
+        top: clientRect.top,
+        bottom: clientRect.bottom
+      };
+    }
+
+    setSizes({
+      screenWidth,
+      screenHeight,
+      ratiowh,
+      ratiohw,
+      rect
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', onResize, false);
+    onResize();
+
+    return () => {
+      window.removeEventListener('resize', onResize, false);
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return sizes;
 };
