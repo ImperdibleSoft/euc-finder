d40311d6c4defcb810b97c3db8538ab9cd71e0c5
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-01-20 11:37:07 +0100

    _document metas now use BRAND_COLOR instead of hardcoded value

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
 import { FB_APP_ID } from '../constants';
+import { BRAND_COLOR } from '../styles/theme';
 
 class MyDocument extends Document {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -19,7 +20,7 @@ class MyDocument extends Document {
           
           <link rel="manifest" href="/manifest.json" />
           <link rel="shortcut icon" href="/favicon/maskable_icon_x512.png" />
-          <meta name="theme-color" content="#2b2d42" />
+          <meta name="theme-color" content={ BRAND_COLOR } />
           <script
             async
             src="https://cdn.jsdelivr.net/npm/pwacompat"
