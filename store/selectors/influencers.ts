89d670e7450cfddf96faf8948749c7825b09a3db
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-03-19 19:24:15 +0100

    Sort influencers by name

--- store/selectors/influencers.ts
+++ store/selectors/influencers.ts
@@ -1,14 +1,19 @@
 import { InfluencerId } from '../../types';
 import { RootState } from '../types';
 
-export const getSponsoredInfluencers = ({ influencers }: RootState) =>
-  influencers.collection.filter(influencer => influencer.sponsored);
+export const getSponsoredInfluencers = ({ influencers }: RootState) => {
+  const sponsored = influencers.collection.filter(influencer => influencer.sponsored);
+  return sponsored;
+};
 
-export const getInfluencers = (rootState: RootState) =>
-  [
+export const getInfluencers = (rootState: RootState) => {
+  const nonSponsored = rootState.influencers.collection.filter(influencer => !influencer.sponsored);
+
+  return [
     ...getSponsoredInfluencers(rootState),
-    ...rootState.influencers.collection.filter(influencer => !influencer.sponsored)
+    ...nonSponsored
   ];
+};
 
 export const getInfluencerById = (id: InfluencerId) =>
   ({ influencers }: RootState) =>
