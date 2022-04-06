81f77b1d147d18c55f1407acf2463a56dc875de5
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-04-06 15:51:03 +0200

    Add footer to the LandingPage

--- components/Screens/LandingPage/index.tsx
+++ components/Screens/LandingPage/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import landingSections from '../../../constants/landingSections';
 import { useLandingTranslations } from '../../../hooks';
 import { TranslationFile } from '../../../types';
+import Footer from '../../Layouts/Footer';
 import LandingSection from './Section';
 
 const layoutNs = { ns: TranslationFile.layout };
@@ -11,20 +12,24 @@ const LandingPage = () => {
   const { t } = useLandingTranslations();
 
   return (
-    <Box sx={ { my: -3 } }>
-      { landingSections.map(section => (
-        <LandingSection
-          key={ section.path }
-          callToAction={ t(section.callToAction) }
-          description={ t(section.description, { appName: t(section.title, layoutNs) }) }
-          extraText={ section.extraText ? t(section.extraText, { appName: t(section.title, layoutNs) }) : undefined }
-          path={ section.path }
-          picture={ section.picture }
-          sx={ section.sx }
-          title={ t(section.title, { ns: TranslationFile.layout }) }
-        />
-      )) }
-    </Box>
+    <>
+      <Box sx={ { my: -3 } }>
+        { landingSections.map(section => (
+          <LandingSection
+            key={ section.path }
+            callToAction={ t(section.callToAction) }
+            description={ t(section.description, { appName: t(section.title, layoutNs) }) }
+            extraText={ section.extraText ? t(section.extraText, { appName: t(section.title, layoutNs) }) : undefined }
+            path={ section.path }
+            picture={ section.picture }
+            sx={ section.sx }
+            title={ t(section.title, { ns: TranslationFile.layout }) }
+          />
+        )) }
+      </Box>
+      
+      <Footer />
+    </>
   );
 };
 
