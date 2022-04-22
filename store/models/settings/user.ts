a470289fd88dc576765ce7455dc6a6b908473fbd
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-04-22 14:45:51 +0200

    Get user's weight from localStorage

--- store/models/settings/user.ts
+++ store/models/settings/user.ts
@@ -1,1 +1,4 @@
-export const getUserInitialState = () => ({ weight: 80 });
+import { LOCAL_STORAGE_KEY } from '../../../types';
+import { getItem } from '../../../utils';
+
+export const getUserInitialState = () => ({ weight: Number(getItem(LOCAL_STORAGE_KEY.USER_WEIGHT)) || 80 });
