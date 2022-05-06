dec82e41f8c90f237914b1903ecd782c87078b6f
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-05-06 12:55:19 +0200

    Add utility to get queryParams

---
+++ utils/routing.ts
@@ -0,0 +1,61 @@
+import { WheelId } from '../types';
+
+export interface AvailableQueryParams {
+  dark: boolean;
+  features?: string[];
+  lang: 'en' | 'es' | 'fr';
+  limits: boolean;
+  picture: boolean;
+  title: boolean;
+  wheelId?: WheelId;
+}
+
+const parseValue = (key: string, value: string) => {
+  switch (key) {
+    case 'features':
+      if (value) {
+        return value.split(',');
+      }
+      return undefined;
+
+    case 'dark':
+    case 'limits':
+    case 'picture':
+    case 'title':
+      return value !== 'false';
+
+    case 'lang':
+      if (value !== 'en' && value !== 'es' && value !== 'fr') {
+        return 'en';
+      }
+
+      return value;
+      
+    default:
+      return value;
+  }
+};
+
+export const getQueryParams = (): AvailableQueryParams => {
+  const location = global?.location ?? {};
+
+  const defaultParams: AvailableQueryParams = {
+    dark: false,
+    lang: 'en',
+    limits: true,
+    picture: true,
+    title: true
+  };
+
+  return (location.search ?? '')
+    .replace(/^\?/, '')
+    .split('&')
+    .reduce((result, curr) => {
+      const [key, value] = curr.split('=');
+    
+      return {
+        ...result,
+        [key]: parseValue(key, value)
+      };
+    }, defaultParams);
+};
