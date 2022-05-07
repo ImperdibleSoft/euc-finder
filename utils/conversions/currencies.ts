664f20894a619ca730467874715f753dfe5f231a
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-05-07 16:26:09 +0200

    Bugfix currencies values

--- utils/conversions/currencies.ts
+++ utils/conversions/currencies.ts
@@ -1,12 +1,12 @@
-const CAD_TO_USD = 0.80;
+const CAD_TO_USD = 0.78;
 export const convertCADsToUSDs = (canadianDolars: number) => canadianDolars * CAD_TO_USD;
 export const convertUSDsToCADs = (canadianDolars: number) => canadianDolars / CAD_TO_USD;
 
-const POUND_TO_EURO = 1.20;
+const POUND_TO_EURO = 1.17;
 export const convertPoundsToEuros = (pounds: number) => pounds * POUND_TO_EURO;
 export const convertEurosToPounds = (pounds: number) => pounds / POUND_TO_EURO;
  
-const USD_TO_EURO = 0.88;
+const USD_TO_EURO = 0.95;
 export const convertUSDsToEuros = (usds: number) => usds * USD_TO_EURO;
 export const convertEurosToUSDs = (euros: number) => euros / USD_TO_EURO;
  
