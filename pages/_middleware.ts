5591daef11af6d2c2d9fd6d53d465765652d1c63
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-06-12 15:08:26 +0200

    Remove _middleware.ts page

--- pages/_middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextFetchEvent, NextRequest } from 'next/server';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const middleware = (req: NextRequest, ev: NextFetchEvent) => {
-  return;
-};
